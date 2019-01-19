ce4d993e5eb9d82003a94f1dae2e7f2d48e3e0a2
CarlosMac <carlosneira1997@gmail.com>
2019-01-19 16:56:12 -0500

    Created Componenets

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import createGame from "./Components/CreateGrame/createGame"
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <header className="App-header">
         <button type="button" class="btn btn-primary">Join Game</button>
         <p> </p>
-        <button type="button" class="btn btn-primary">HI ;) </button>
-
+        <button type="button" class="btn btn-primary">Create Game</button>
         </header>
       </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Components/CreateGrame/createGame.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './createGame.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+class createGame {
+    render(){
+        return(
+            <BrowserRouter>
+            <div className = "createGame-Header">
+
+
+            </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default createGame;
