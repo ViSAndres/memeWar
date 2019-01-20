7263bcd57f97f251d145dcac6f26a556a1e7aa5a
CarlosMac <carlosneira1997@gmail.com>
2019-01-19 22:54:44 -0500

    Format Host Game

--- src/components/hostGame.js
+++ src/components/hostGame.js
@@ -1,5 +1,6 @@
 import React,{Component} from "react";
-
+import "./hostGame.css";
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 class HostGame extends Component {
     constructor(props){
         super(props)
@@ -10,7 +11,25 @@ class HostGame extends Component {
         <div className="App">
         <header>
             <p>
-                Enter the session name
+                Enter the session name:
+            </p>
+            <form action='/'method="POST">
+                <div class="form-group">
+                <label for="exampleFormControlInput1">Session Name</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="D@NK @$$ M3MEz"></input>
+                </div>
+            </form>
+        </header>
+
+        </div>
+    )
+}
+}
+/*
+<div className="App">
+        <header>
+            <p>
+                Enter the session name:
             </p>
             <form action='/'method="POST">
                 <input class="input-form" type="text" name="sessionName"></input>
@@ -20,8 +39,5 @@ class HostGame extends Component {
         </header>
 
         </div>
-    )
-}
-}
-
+*/
 export default HostGame;
