7b813a8ff136f646e3d540881a50d883a3780681
Maurice Barnes <mauriceb2197@gmail.com>
2019-01-20 02:46:00 -0500

    Carlos' magic

--- src/components/hostGame.js
+++ src/components/hostGame.js
@@ -1,12 +1,37 @@
 import React,{Component} from "react";
+import axios from 'axios';
 import "./hostGame.css";
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 class HostGame extends Component {
     constructor(props){
         super(props)
-        
+        this.state = {
+            input: '',
+        }
+       
     }
-    
+
+    getInput = (e) => {
+        this.setState({input: e.target.value});
+    }
+
+    submitHost = async () => {
+        const {input} = this.state;
+
+        if(input.length > 0) {
+            try{
+                const response = await axios.post('http://localhost:3001/host', {
+                    name: input
+                });
+                console.log(response);
+                //redirect here to lobby;
+
+            } catch(e) {
+                console.log(e);
+            }
+        }
+
+    }
     render(){
     
     return(
@@ -16,12 +41,11 @@ class HostGame extends Component {
             <p>
                 Enter the session name:
             </p>
-            <form action='http://localhost:3001/host'method="post">
-                <div class="form-group">
+            <div class="form-group">
                 <label for="exampleFormControlInput1">Session Name</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="D@NK @$$ M3MEz"></input>
-                </div>
-            </form>
+                <input type="text" class="form-control" onChange={this.getInput} id="exampleFormControlInput1" placeholder="D@NK @$$ M3MEz"></input>
+                <button className="btn btn-primary" onClick={this.submitHost}> Submit </button>
+            </div>
         </header>
 
         </div>
